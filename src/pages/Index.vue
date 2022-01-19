c15b890c13750412e25efe93eed64182a027e347
Corina Pelichowski <corina@danitt.com>
2022-01-19 20:47:46 +1000

    error message

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,6 +8,8 @@
       @keyup.esc="clearMessage"
       @keyup.enter="alertMessage"
       v-autofocus
+      :class="{ error: message.length > 22 }"
+      ref="messageInput"
     />
     <button @click="clearMessage">Clear</button>
 
@@ -58,29 +60,10 @@ export default {
       },
     },
   },
-  beforeCreate() {
-    console.log("beforeCreate");
-  },
-  created() {
-    console.log("created");
-  },
-  beforeMount() {
-    console.log("beforeMount");
-  },
+
   mounted() {
-    console.log("mounted");
-  },
-  beforeUpdate() {
-    console.log("beforeUpdate");
-  },
-  updated() {
-    console.log("updated");
-  },
-  beforeDestroy() {
-    console.log("beforeDestroy");
-  },
-  destroyed() {
-    console.log("destroyed");
+    console.log(this.$refs);
+    this.$refs.messageInput.className = "bg-green";
   },
 };
 </script>
