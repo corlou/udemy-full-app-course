75136fe828d9adb208a467e57156d2f244652e60
Corina Pelichowski <corina@danitt.com>
2022-01-20 17:59:40 +1000

    keys

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,7 @@
     <ul>
       <task
         v-for="(task, index) in tasks"
-        :key="task.tasks"
+        :key="task.id"
         :task="task"
         :index="index"
         >{{ task.name }}
@@ -18,16 +18,19 @@ export default {
     return {
       tasks: [
         {
+          id: 1,
           name: "Go to shop",
           dueDate: "2019/05/12",
           dueTime: "18:30",
         },
         {
+          id: 2,
           name: "Get bananas",
           dueDate: "2019/05/13",
           dueTime: "14:00",
         },
         {
+          id: 3,
           name: "Get apples",
           dueDate: "2019/05/14",
           dueTime: "16:00",
